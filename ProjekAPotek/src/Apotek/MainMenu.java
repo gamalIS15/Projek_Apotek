a0bfdb75d5a09f488656d7d463bf12e1a64df343
ahsanulmarom <ahsanulmarom@gmail.com>
2017-01-12 23:52:11 +0700

    120117 - 2351

--- ProjekAPotek/src/Apotek/MainMenu.java
+++ ProjekAPotek/src/Apotek/MainMenu.java
@@ -20,7 +20,6 @@ import javax.swing.Timer;
  * @author user
  */
 public class MainMenu extends javax.swing.JFrame {
-    String jamSekarang;
     /**
      * Creates new form MainMenu
      */
@@ -346,6 +345,32 @@ public class MainMenu extends javax.swing.JFrame {
             }
         });
     }
+    
+    public final void setWaktu() {
+        ActionListener taskPerformer = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nol_jam = "", nol_menit = "", nol_detik = "";
+                
+                java.util.Date dateTime = new java.util.Date();
+                int nilai_jam = dateTime.getHours();
+                int nilai_menit = dateTime.getMinutes();
+                int nilai_detik = dateTime.getSeconds();
+                
+                if(nilai_jam <= 9) nol_jam="0";
+                if(nilai_menit <= 9) nol_menit="0";
+                if(nilai_detik <= 9) nol_detik="0";
+                
+                String waktu = nol_jam + Integer.toString(nilai_jam);
+                String menit = nol_menit + Integer.toString(nilai_menit);
+                String detik = nol_detik + Integer.toString(nilai_detik);
+                
+                txtTanggal.setText(String.valueOf(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))) + " " + 
+                        waktu + ":" + menit + ":" + detik);
+            }
+        };
+        new Timer(1000, taskPerformer).start();
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel btnBantuan;
@@ -357,7 +382,7 @@ public class MainMenu extends javax.swing.JFrame {
     private PanelTransparan.ClPanelTransparan clPanelTransparan1;
     private PanelTransparan.ClPanelTransparan clPanelTransparan2;
     private javax.swing.JPanel jPanel1;
-    public javax.swing.JLabel txtTanggal;
+    private javax.swing.JLabel txtTanggal;
     private javax.swing.JLabel txtWelcome;
     // End of variables declaration//GEN-END:variables
 }
