28a7b635d0cf433b083c9ed82e49a7053dd72831
gamalIS15 <gamaltn@gmail.com>
2017-01-17 13:53:41 +0700

    Perubahan pada resep :)

--- ProjekAPotek/src/Apotek/Resep.java
+++ ProjekAPotek/src/Apotek/Resep.java
@@ -72,6 +72,9 @@ public class Resep extends javax.swing.JFrame {
         }
     }
     
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
