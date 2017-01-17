a616c395062ea1a0003be8bec1b03b2591fb281e
gamalIS15 <gamaltn@gmail.com>
2017-01-17 13:43:55 +0700

    Resep_set

--- ProjekAPotek/src/Apotek/setResep.java
+++ ProjekAPotek/src/Apotek/setResep.java
@@ -19,14 +19,14 @@ public class setResep {
     private String jenisLayanan;
     private String bpjs_nonBpjs;
     private String namaObat;
-    private String jumlahObat;
+    private int jumlahObat;
 
     public setResep() {
     }
     
     
     
-    public setResep(Date tanggal, String namaPasien, String usia, String alamat, String jenisLayanan, String bpjs_nonBpjs, String namaObat, String jumlahObat) {
+    public setResep(Date tanggal, String namaPasien, String usia, String alamat, String jenisLayanan, String bpjs_nonBpjs, String namaObat, int jumlahObat) {
         this.tanggal = tanggal;
         this.namaPasien = namaPasien;
         this.usia = usia;
@@ -95,11 +95,11 @@ public class setResep {
         this.namaObat = namaObat;
     }
 
-    public String getJumlahObat() {
+    public int getJumlahObat() {
         return jumlahObat;
     }
 
-    public void setJumlahObat(String jumlahObat) {
+    public void setJumlahObat(int jumlahObat) {
         this.jumlahObat = jumlahObat;
     }
     
